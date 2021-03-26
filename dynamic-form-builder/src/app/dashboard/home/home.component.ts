ebf5feec84a5d365245cd224e2270a3277176c78
Yash Shah <yash_9999@yahoo.com>
2021-03-26 19:57:34 -0400

    Add: Controls for input type attributes

--- dynamic-form-builder/src/app/dashboard/home/home.component.ts
+++ dynamic-form-builder/src/app/dashboard/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HomeService } from './home.service';
 import { PrimeNGConfig } from 'primeng/api';
 import { HttpClient } from '@angular/common/http';
@@ -7,13 +7,27 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 
 export class HomeComponent implements OnInit {
   // var user = 1;
-  
+  color = 'orange';
+  defaultCard = {
+    'background-color': 'orange',
+    'margin-bottom': '2em',
+    'width': '200px',
+    'height': '90px',
+    'border-radius': '25px',
+    'text-align': 'center',
+    'padding-top': '70px',
+    'color': 'white',
+    'font-weight': 'bold',
+    'margin-left': '50px',
+    'margin-right': '40px'
+  };
   dataform: any;
   isDataformEmpty = false;
   constructor(
@@ -24,14 +38,15 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
+    this.isDataformEmpty = false;
     this.callTemplateApiByUserId();  
   }
 
   callTemplateApiByUserId(){
     this.homeService.getTemplateByUserId().subscribe(async (response: any) => {        
       if (response && response.error && response.error.length === 0) {
-        this.dataform = (response.data) ? response.data : [] ;
-        if(this.dataform === []){
+        this.dataform = response.data;
+        if(this.dataform.length === 0){
           this.isDataformEmpty = true;
         }
       }
